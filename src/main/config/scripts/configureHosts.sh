41cc7e8902bc7c47510ddb9f2fa5d9216c9b7ad4
Aniket Chakraborty <aniket.chakraborty@hashedin.com>
2020-07-09 00:32:54 +0530

    Added Appspec and scripts

---
+++ src/main/config/scripts/configureHosts.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#install java
+sudo apt install -y openjdk-11-jre-headless
+sudo apt install -y maven
+
+
+
---
+++ src/main/config/scripts/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# kill any running java instance
+echo Killing any other java process
+pid = ps -a | grep java | awk '{print $1}'
+kill -9 $pid
+echo Step completed
+
+#start application
+java -jar /home/ubuntu/installables/target/TestAWSCodePipeline-1.0-SNAPSHOT.jar
