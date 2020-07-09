bf8c4d982c99fe9466d35c057f9d08f31cfa5d25
Aniket Chakraborty <aniket.chakraborty@hashedin.com>
2020-07-09 17:43:38 +0530

    Change in java command 7

--- src/main/config/scripts/deploy.sh
+++ src/main/config/scripts/deploy.sh
@@ -13,9 +13,9 @@ sudo cp /home/ubuntu/installables/test-app.service /etc/systemd/system/
 sudo systemctl daemon-reload
 
 # Stop the gofit-server service
-sudo service test-app.service stop
+sudo service test-app stop
 
 
 #start application
 #nohup java -jar test-app.jar > /dev/null 2> /dev/null < /dev/null &
-sudo service test-app.service start
+sudo service test-app start
