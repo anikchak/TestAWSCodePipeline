603751c4fcfea4a02dcb181565aecab9d0c8dd75
Aniket Chakraborty <aniket.chakraborty@hashedin.com>
2020-07-09 17:39:16 +0530

    Change in java command 6

--- src/main/config/scripts/deploy.sh
+++ src/main/config/scripts/deploy.sh
@@ -7,15 +7,15 @@ kill -9 $pid
 echo Step completed
 
 # Updating system.service file
-sudo cp /home/ubuntu/installables/test-app-service /etc/systemd/system/
+sudo cp /home/ubuntu/installables/test-app.service /etc/systemd/system/
 
 # Update the system-daemon
 sudo systemctl daemon-reload
 
 # Stop the gofit-server service
-sudo service test-app stop
+sudo service test-app.service stop
 
 
 #start application
 #nohup java -jar test-app.jar > /dev/null 2> /dev/null < /dev/null &
-sudo service test-app start
+sudo service test-app.service start
